dbe3dc184e36b6cf510437edf6880debf2c9c1fe
Tanmay Mudholkar <tkmudholkar@gmail.com>
2021-06-29 09:56:58 -0500

    In rare instances, jobs crash because of some issue with the detector geometry in GEANT. The code is now able to check while submitting jobs whether the seed corresponding to a job is masked, and if so, to choose a different seed.

--- py_condor_submission_scripts/run_pi0_generation.py
+++ py_condor_submission_scripts/run_pi0_generation.py
@@ -29,6 +29,11 @@ e2e_common.update_and_upload_e2e_tarballs()
 # Create and submit jobs
 for mass_point_title in mass_points:
     mass = mass_points[mass_point_title]
+    masked_seeds = None
+    try:
+        masked_seeds = settings.values_["generate_pi0"]["masked_seeds"][mass_point_title]
+    except KeyError:
+        masked_seeds = []
     for copy_index in range(settings.values_["generate_pi0"]["nCopiesPerMassPoint"]):
         processIdentifier = "generate_pi0_{t}_copy{i}".format(t=mass_point_title, i=copy_index)
         filesToTransfer = [e2e_env.x509Proxy, "{er}/sh_snippets/setup_environment_remote.sh".format(er=e2e_env.e2e_root)]
@@ -38,7 +43,12 @@ for mass_point_title in mass_points:
         jdlInterface.addScriptArgument("{mpt}_copy{i}".format(mpt=mass_point_title, i=copy_index)) # Argument 2: identifier
         jdlInterface.addScriptArgument("{ep}/{eer}/genAOD/pi0".format(ep=e2e_env.eos_prefix, eer=e2e_env.e2e_eos_root)) # Argument 3: output EOS directory with prefix for xrdcp
         jdlInterface.addScriptArgument("{m:.4f}".format(m=mass)) # Argument 4: mass
-        jdlInterface.addScriptArgument("{s}".format(s=(12345+copy_index))) # Argument 5: random number generator seed
+        seed = 12345 + copy_index
+        if seed in masked_seeds:
+            new_seed = seed + settings.values_["generate_pi0"]["nCopiesPerMassPoint"]
+            print("Seed {s1} masked; trying {s2}".format(s1=seed, s2=new_seed))
+            seed = new_seed
+        jdlInterface.addScriptArgument("{s}".format(s=seed)) # Argument 5: random number generator seed
         if (e2e_env.habitat == "lxplus"):
             jdlInterface.setFlavor("tomorrow")
         # # Write JDL
